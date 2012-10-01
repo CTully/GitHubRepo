bd587cd7407325a4882256f38b03148593df9348
Chris Tully <anthology_12@hotmail.com>
2012-10-01 11:46:52 -0500

    Commented
    Iterator, fully commented.

--- Weeks/week4/oop/Iterator/myClass.php
+++ Weeks/week4/oop/Iterator/myClass.php
@@ -31,8 +31,14 @@ class MyClass
 
 class MyIterator implements Iterator
 {
+
 	private $var = array();
 	
+	/* Function: __construct()
+	*Accepts: $array---typeof(int);
+	*Returns: ...technically it constructs a new object... but as a return type.. nothing..I think...
+	*Purpose: To show the example of iteration
+	*/
 	public function __construct($array)
 	{
 		if (is_array($array))
@@ -41,12 +47,22 @@ class MyIterator implements Iterator
 		}//end if
 	}//end __construct()
 
+	/* Function: rewind()
+	*Accepts: n/a
+	*Returns: this reset...
+	*Purpose: To show the example of iteration
+	*/
 	public function rewind()
 	{
 		echo'Rewinding \n';
 		reset($this ->var);
 	}
 	
+	/* Function: current()
+	*Accepts: N/A
+	*Returns: current position
+	*Purpose: To show the example of iteration
+	*/
 	public function current()
 	{
 	$var = current($this ->var);
@@ -54,6 +70,11 @@ class MyIterator implements Iterator
 	return $var;
 	}
 	
+	/* Function: key()
+	*Accepts: N/A
+	*Returns: A key..?
+	*Purpose: To show the example of iteration
+	*/
 	public function key()
 	{
 		$var=key($this->var);
@@ -61,6 +82,11 @@ class MyIterator implements Iterator
 		return $var;
 	}
 	
+	/* Function: next()
+	*Accepts: N/A
+	*Returns: the next variable
+	*Purpose: To show the example of iteration
+	*/
 	public function next()
 	{
 	$var =next($this->var);
@@ -68,6 +94,11 @@ class MyIterator implements Iterator
 	return $var;
 	}
 	
+	/* Function:  valid()
+	*Accepts: N/A
+	*Returns: establishes a valadity function.
+	*Purpose: To show the example of iteration
+	*/
 	public function valid()
 	{
 		$key = key($this->var);
